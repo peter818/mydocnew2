d1a0f540e5ca18d7372e1dc5d395f8c592ffe1e3
cyfung818 <file11112002@yahoo.com.hk>
2024-09-06 09:06:14 +0800

    Step 12 : create pages app

--- listings/admin.py
+++ listings/admin.py
@@ -1,14 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Listing
+from .models import Listing,Realtor
 
 class ListingAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'is_published', 'prices', 'list_date', 'realtor')
-    list_display_links = ('id,' 'title')
+    list_display_links = ('id', 'title')
     list_filter = ('realtor',)
-    list_editable = ('is_published',)
+    list_editable = ('is_published',)  # 确保 'is_published' 在 'list_display' 中
     search_fields = ('title', 'description', 'address', 'price')
     list_per_page = 25
     
-admin.site.register(Listing, ListingAdmin)
+    def prices(self, obj):
+        return obj.prices
+    
+admin.site.register(Listing, ListingAdmin)  
+    
+#class ListingAdmin(admin.ModelAdmin):
+    #list_display = ('id', 'title', 'is_published', 'prices', 'list_date', 'realtor')
+    #list_display_links = ('id,' 'title')
+    #list_display_links = None
+    #list_filter = ('realtor',)
+    #list_editable = ('is_published',)
+    #search_fields = ('title', 'description', 'address', 'price')
+    #list_per_page = 25
+    
+
--- realtors/admin.py
+++ realtors/admin.py
@@ -5,7 +5,7 @@ from .models import Realtor
 
 class RealtorAdmin(admin.ModelAdmin):
     list_display = ('id', 'name','email', 'hire_date')
-    list_display_links = ('id,' 'name')
+    list_display_links = ('id', 'name')
     search_fields = ('name',)
     list_per_page = 25
     
